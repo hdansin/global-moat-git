4a38b30d783f26558eebbbf1859d50be2d35d5e1
Glitch (global-moat) <none>
2019-10-23 20:22:14 +0000

    📖🐣 Checkpoint ./server.js:7955134/115

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/timestamp", function(req, res) {
+  
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
